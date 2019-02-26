ea00ada2f198b90849e787497a62b59da7b6b65e
makicamel <unright@gmail.com>
2019-02-26 22:53:10 +0900

    Create addTodo method.

--- src/App.js
+++ src/App.js
@@ -11,8 +11,25 @@ class App extends Component {
       ],
       uniqueId: 1,
     };
-
   }
+
+  addTodo(title){
+    const {
+      tasks,
+      uniqueId,
+    } = this.state;
+
+    tasks.push({
+      title,
+      id: uniqueId,
+    });
+
+    this.setState({
+      tasks,
+      uniqueId: uniqueId + 1,
+    })
+  }
+
   render(){
     return (
       <div>
