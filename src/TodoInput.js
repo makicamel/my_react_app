c0ef62593783e8b1886beff315ad0152abe017df
makicamel <unright@gmail.com>
2019-02-27 08:26:48 +0900

    Add handleClick method to TodoInput component.

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -1,11 +1,18 @@
 import React, {Component} from 'react';
 
 class TodoInput extends Component {
+  constructor(props){
+    super(props);
+    // this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick = (props) => {
+    this.props.addTodo('new TODO');
+  }
   render(){
     return (
       <div>
         <input placeholder="TODOを入力してください" />
-        <button>登録</button>
+        <button onClick={this.handleClick}>登録</button>
       </div>
     );
   }
